7be1308228592fe54c05f0c851f38551c4f2b73d
jasheer abuthahir <ajasheer@gmail.com>
2021-09-15 08:19:17 -0400

    First TDD change

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,21 @@
 package org.xpdojo.bank;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Matches;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+       // assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance, Matchers.is(100));
     }
+
 }
